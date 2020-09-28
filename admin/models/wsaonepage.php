2a7d2f5419e309fc3fbd3570e7bc87f772f67829
bram waasdorp <bramwaas@gmail.com>
2020-09-28 17:21:35 +0200

    edit not ready

--- admin/models/wsaonepage.php
+++ admin/models/wsaonepage.php
@@ -5,6 +5,7 @@
  *
  * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * 28-9-2020
  */
 
 // No direct access to this file
@@ -17,72 +18,115 @@ defined('_JEXEC') or die('Restricted access');
  */
 class WsaOnePageModelWsaOnePage extends JModelAdmin
 {
-	/**
-	 * Method to get a table object, load it if necessary.
-	 *
-	 * @param   string  $type    The table name. Optional.
-	 * @param   string  $prefix  The class prefix. Optional.
-	 * @param   array   $config  Configuration array for model. Optional.
-	 *
-	 * @return  JTable  A JTable object
-	 *
-	 * @since   1.6
-	 */
-	public function getTable($type = 'WsaOnePage', $prefix = 'WsaOnePageTable', $config = array())
-	{
-		return JTable::getInstance($type, $prefix, $config);
-	}
-
-	/**
-	 * Method to get the record form.
-	 *
-	 * @param   array    $data      Data for the form.
-	 * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
-	 *
-	 * @return  mixed    A JForm object on success, false on failure
-	 *
-	 * @since   1.6
-	 */
-	public function getForm($data = array(), $loadData = true)
-	{
-		// Get the form.
-		$form = $this->loadForm(
-			'com_wsaonepage.wsaonepage',
-			'wsaonepage',
-			array(
-				'control' => 'jform',
-				'load_data' => $loadData
-			)
-		);
-
-		if (empty($form))
-		{
-			return false;
-		}
-
-		return $form;
-	}
-
-	/**
-	 * Method to get the data that should be injected in the form.
-	 *
-	 * @return  mixed  The data for the form.
-	 *
-	 * @since   1.6
-	 */
-	protected function loadFormData()
-	{
-		// Check the session for previously entered form data.
-		$data = JFactory::getApplication()->getUserState(
-			'com_wsaonepage.edit.wsaonepage.data',
-			array()
-		);
-
-		if (empty($data))
-		{
-			$data = $this->getItem();
-		}
-
-		return $data;
-	}
+    /**
+     * Method to get a table object, load it if necessary.
+     *
+     * @param   string  $type    The table name. Optional.
+     * @param   string  $prefix  The class prefix. Optional.
+     * @param   array   $config  Configuration array for model. Optional.
+     *
+     * @return  JTable  A JTable object
+     *
+     * @since   1.6
+     */
+    public function getTable($type = 'WsaOnePage', $prefix = 'WsaOnePageTable', $config = array())
+    {
+        return JTable::getInstance($type, $prefix, $config);
+    }
+    
+    /**
+     * Method to get the record form.
+     *
+     * @param   array    $data      Data for the form.
+     * @param   boolean  $loadData  True if the form is to load its own data (default case), false if not.
+     *
+     * @return  mixed    A JForm object on success, false on failure
+     *
+     * @since   1.6
+     */
+    public function getForm($data = array(), $loadData = true)
+    {
+        // Get the form.
+        $form = $this->loadForm(
+            'com_wsaonepage.wsaonepage',
+            'wsaonepage',
+            array(
+                'control' => 'jform',
+                'load_data' => $loadData
+            )
+            );
+        
+        if (empty($form))
+        {
+            return false;
+        }
+        // Determine correct permissions to check.
+        if ($this->getState('wsaonepage.id'))
+        {
+            // Existing record. Can only edit in selected categories.
+            $form->setFieldAttribute('catid', 'action', 'core.edit');
+        }
+        else
+        {
+            // New record. Can only create in selected categories.
+            $form->setFieldAttribute('catid', 'action', 'core.create');
+        }
+        
+        // Modify the form based on access controls.
+        if (!$this->canEditState((object) $data))
+        {
+            // Disable fields for display.
+            $form->setFieldAttribute('ordering', 'disabled', 'true');
+            $form->setFieldAttribute('publish_up', 'disabled', 'true');
+            $form->setFieldAttribute('publish_down', 'disabled', 'true');
+            $form->setFieldAttribute('state', 'disabled', 'true');
+            $form->setFieldAttribute('sticky', 'disabled', 'true');
+            
+            // Disable fields while saving.
+            // The controller has already verified this is a record you can edit.
+            $form->setFieldAttribute('ordering', 'filter', 'unset');
+            $form->setFieldAttribute('publish_up', 'filter', 'unset');
+            $form->setFieldAttribute('publish_down', 'filter', 'unset');
+            $form->setFieldAttribute('state', 'filter', 'unset');
+            $form->setFieldAttribute('sticky', 'filter', 'unset');
+        }
+        
+        
+        
+        return $form;
+    }
+    
+    /**
+     * Method to get the data that should be injected in the form.
+     *
+     * @return  mixed  The data for the form.
+     *
+     * @since   1.6
+     */
+    protected function loadFormData()
+    {
+        // Check the session for previously entered form data.
+        $data = JFactory::getApplication()->getUserState(
+            'com_wsaonepage.edit.wsaonepage.data',
+            array()
+            );
+        
+        if (empty($data))
+        {
+            $data = $this->getItem();
+            /* TODO is from banners.php is this a good idea ?
+             // Prime some default values.
+             if ($this->getState('banner.id') == 0)
+             {
+             $filters     = (array) $app->getUserState('com_banners.banners.filter');
+             $filterCatId = isset($filters['category_id']) ? $filters['category_id'] : null;
+             
+             $data->set('catid', $app->input->getInt('catid', $filterCatId));
+             }
+             */
+            
+        }
+        
+        return $data;
+    }
 }
--- admin/views/wsaonepage/tmpl/edit.php
+++ admin/views/wsaonepage/tmpl/edit.php
@@ -5,27 +5,88 @@
  *
  * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ * 28-9-2020
  */
 
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 
+JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
+
+JHtml::_('jquery.framework');
+JHtml::_('behavior.formvalidator');
+JHtml::_('formbehavior.chosen', '#jform_catid', null, array('disable_search_threshold' => 0 ));
+JHtml::_('formbehavior.chosen', 'select');
+// TODO from banners is this usefull?
+/*
+ JFactory::getDocument()->addScriptDeclaration('
+ Joomla.submitbutton = function(task)
+ {
+ if (task == "banner.cancel" || document.formvalidator.isValid(document.getElementById("banner-form")))
+ {
+ Joomla.submitform(task, document.getElementById("banner-form"));
+ }
+ };
+ jQuery(document).ready(function ($){
+ $("#jform_type").on("change", function (a, params) {
+ 
+ var v = typeof(params) !== "object" ? $("#jform_type").val() : params.selected;
+ 
+ var img_url = $("#image, #url");
+ var custom  = $("#custom");
+ 
+ switch (v) {
+ case "0":
+ // Image
+ img_url.show();
+ custom.hide();
+ break;
+ case "1":
+ // Custom
+ img_url.hide();
+ custom.show();
+ break;
+ }
+ }).trigger("change");
+ });
+ ');
+ */
 ?>
+
 <form action="<?php echo JRoute::_('index.php?option=com_wsaonepage&layout=edit&id=' . (int) $this->item->id); ?>"
-    method="post" name="adminForm" id="adminForm">
+    method="post" name="adminForm" id="wsaonepage-form" class="form-validate">
     <div class="form-horizontal">
-        <fieldset class="adminform">
-            <legend><?php echo JText::_('COM_WSAONEPAGE_WSAONEPAGE_DETAILS'); ?></legend>
+		<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'details')); ?>
+		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'details', JText::_('COM_WSAONEPAGE_WSAONEPAGE_DETAILS')); ?>
             <div class="row-fluid">
                 <div class="span6">
-                    <?php 
-                        foreach($this->form->getFieldset() as $field) {
-                            echo $field->renderField();        
-                        }
-                    ?>
+					<?php echo $this->form->renderFieldset('details'); ?>
                 </div>
             </div>
-        </fieldset>
+		<?php echo JHtml::_('bootstrap.endTab'); ?>
+
+
+
+
+
+		<?php //echo JHtml::_('bootstrap.addTab', 'myTab', 'otherparams', JText::_('COM_BANNERS_GROUP_LABEL_BANNER_DETAILS')); ?>
+		<?php //echo $this->form->renderFieldset('otherparams'); ?>
+		<?php //echo JHtml::_('bootstrap.endTab'); ?>
+		
+		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'publishing', JText::_('JGLOBAL_FIELDSET_PUBLISHING')); ?>
+		<div class="row-fluid form-horizontal-desktop">
+			<div class="span6">
+				<?php echo JLayoutHelper::render('joomla.edit.publishingdata', $this); ?>
+			</div>
+			<div class="span6">
+				<?php echo $this->form->renderFieldset('metadata'); ?>
+			</div>
+		</div>
+		<?php echo JHtml::_('bootstrap.endTab'); ?>
+
+		<?php echo JHtml::_('bootstrap.endTabSet'); ?>
+
+
     </div>
     <input type="hidden" name="task" value="wsaonepage.edit" />
     <?php echo JHtml::_('form.token'); ?>
