db79c16fcc421d02fe2f743494451d0f6e67b964
bram waasdorp <bramwaas@gmail.com>
2020-10-03 19:27:12 +0200

    .

--- admin/views/wsaonepage/tmpl/edit.php
+++ admin/views/wsaonepage/tmpl/edit.php
@@ -18,50 +18,52 @@ JHtml::_('behavior.formvalidator');
 JHtml::_('formbehavior.chosen', '#jform_catid', null, array('disable_search_threshold' => 0 ));
 JHtml::_('formbehavior.chosen', 'select');
 // TODO from banners is this usefull?
-/*
- JFactory::getDocument()->addScriptDeclaration('
- Joomla.submitbutton = function(task)
- {
- if (task == "banner.cancel" || document.formvalidator.isValid(document.getElementById("banner-form")))
- {
- Joomla.submitform(task, document.getElementById("banner-form"));
- }
- };
- jQuery(document).ready(function ($){
- $("#jform_type").on("change", function (a, params) {
- 
- var v = typeof(params) !== "object" ? $("#jform_type").val() : params.selected;
- 
- var img_url = $("#image, #url");
- var custom  = $("#custom");
- 
- switch (v) {
- case "0":
- // Image
- img_url.show();
- custom.hide();
- break;
- case "1":
- // Custom
- img_url.hide();
- custom.show();
- break;
- }
- }).trigger("change");
- });
- ');
- */
-?>
 
+JFactory::getDocument()->addScriptDeclaration('
+	Joomla.submitbutton = function(task)
+	{
+		if (task == "wsaonepage.cancel" || document.formvalidator.isValid(document.getElementById("wsaonepage-form")))
+		{
+			Joomla.submitform(task, document.getElementById("wsaonepage-form"));
+		}
+	};
+	jQuery(document).ready(function ($){
+		$("#jform_type").on("change", function (a, params) {
+    
+			var v = typeof(params) !== "object" ? $("#jform_type").val() : params.selected;
+    
+			var img_url = $("#image, #url");
+			var custom  = $("#custom");
+    
+			switch (v) {
+				case "0":
+					// Image
+					img_url.show();
+					custom.hide();
+					break;
+				case "1":
+					// Custom
+					img_url.hide();
+					custom.show();
+					break;
+			}
+		}).trigger("change");
+	});
+');
+
+?>
 <form action="<?php echo JRoute::_('index.php?option=com_wsaonepage&layout=edit&id=' . (int) $this->item->id); ?>"
     method="post" name="adminForm" id="wsaonepage-form" class="form-validate">
     <div class="form-horizontal">
 		<?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'details')); ?>
 		<?php echo JHtml::_('bootstrap.addTab', 'myTab', 'details', JText::_('COM_WSAONEPAGE_WSAONEPAGE_DETAILS')); ?>
             <div class="row-fluid">
-                <div class="span6">
+                <div class="span9">
 					<?php echo $this->form->renderFieldset('details'); ?>
                 </div>
+				<div class="span3">
+					<?php echo JLayoutHelper::render('joomla.edit.global', $this); ?>
+				</div>
             </div>
 		<?php echo JHtml::_('bootstrap.endTab'); ?>
 
@@ -69,7 +71,7 @@ JHtml::_('formbehavior.chosen', 'select');
 
 
 
-		<?php //echo JHtml::_('bootstrap.addTab', 'myTab', 'otherparams', JText::_('COM_BANNERS_GROUP_LABEL_BANNER_DETAILS')); ?>
+		<?php //echo JHtml::_('bootstrap.addTab', 'myTab', 'otherparams', JText::_('COM_WSAONEPAGE_GROUP_LABEL_WSAONEPAGE_DETAILS')); ?>
 		<?php //echo $this->form->renderFieldset('otherparams'); ?>
 		<?php //echo JHtml::_('bootstrap.endTab'); ?>
 		
