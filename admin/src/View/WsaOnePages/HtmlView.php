dee66fd7d09ca4df5a4382676eef8e325d6009e5
bram waasdorp <bramwaas@gmail.com>
2021-08-14 10:46:56 +0200

    0.6.1 HtmlView list

--- admin/src/View/WsaOnePages/HtmlView.php
+++ admin/src/View/WsaOnePages/HtmlView.php
@@ -10,13 +10,29 @@ namespace WaasdorpSoekhan\Component\WsaOnePage\Administrator\View\WsaOnePages;
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\ContentHelper;
+use Joomla\CMS\Language\Multilanguage;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\MVC\View\GenericDataException;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+use Joomla\CMS\Toolbar\Toolbar;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+
 
 
 /**
  * Main "WsaOnePages" Admin View
  */
 class HtmlView extends BaseHtmlView {
+ 
+    /**
+     * Is this view an Empty State
+     *
+     * @var  boolean
+     * @since 4.0.0
+     */
+    private $isEmptyState = false;
     
     /**
      * Display the WsaOnePage view
@@ -28,10 +44,6 @@ class HtmlView extends BaseHtmlView {
     function display($tpl = null)
     {
         
-        // Get application
-        $app = JFactory::getApplication();
-        $context = "wsaonepage.list.admin.wsaonepage";
-        
         // Get data from the model
         $this->items		= $this->get('Items');
         $this->pagination	= $this->get('Pagination');
@@ -40,23 +52,27 @@ class HtmlView extends BaseHtmlView {
         //		$this->filter_order_Dir = $app->getUserStateFromRequest($context.'filter_order_Dir', 'filter_order_Dir', 'asc', 'cmd');
         $this->filterForm    	= $this->get('FilterForm');
         $this->activeFilters 	= $this->get('ActiveFilters');
-        
+
+        if (!\count($this->items) && $this->isEmptyState = $this->get('IsEmptyState'))
+        {
+            $this->setLayout('emptystate');
+        }
         
         // Check for errors.
-        if (count($errors = $this->get('Errors')))
+        if (\count($errors = $this->get('Errors')))
         {
-            JError::raiseError(500, implode('<br />', $errors));
-            
-            return false;
+            throw new GenericDataException(implode("\n", $errors), 500);
         }
+        
+        
         // Set the toolbar and number of found items
         $this->addToolBar();
         
         // Display the template
         parent::display($tpl);
         
-        // Set the document
-        $this->setDocument();
+        // TODO Set the document removed for J4 but in examples also not avail in J3
+         $this->setDocument();
     }
     /**
      * Add the page title and toolbar.
@@ -67,33 +83,71 @@ class HtmlView extends BaseHtmlView {
      */
     protected function addToolBar()
     {
-        $input = JFactory::getApplication()->input;
+        $input = Factory::getApplication()->input;
         // TODO	    $canDo = JHelperContent::getActions('com_content', 'category', $this->state->get('filter.category_id'));
         
         // Hide Joomla Administrator Main menu TODO seems not a good idea because there is no other option to close this screen
         //	    $input->set('hidemainmenu', true);
         
         $isNew = ($this->item->id == 0);
-        $title = JText::_('COM_WSAONEPAGE_MANAGER_WSAONEPAGES');
+
+        // Get the toolbar object instance
+        $toolbar = Toolbar::getInstance('toolbar');
+        ToolbarHelper::title(Text::_('COM_WSAONEPAGE_MANAGER_WSAONEPAGES'), 'wsaonepage');
+        $toolbar->addNew('wsaonepage.add');
+        $toolbar->edit('wsaonepage.edit');
+        $toolbar->delete('wsaonepages.delete')
+        ->text('JTOOLBAR_EMPTY_TRASH')
+        ->message('JGLOBAL_CONFIRM_DELETE')
+        ->listCheck(true);
+        $toolbar->preferences('com_wsaonepages');
         
-        if ($this->pagination->total)
-        {
-            $title .= "<span style='font-size: 0.5em; vertical-align: middle;'>(" . $this->pagination->total . ")</span>";
-        }
         
-        JToolBarHelper::title($title, 'wsaonepage');
-        JToolBarHelper::addNew('wsaonepage.add');
-        JToolBarHelper::editList('wsaonepage.edit');
-        /* TODo werkt nog niet $cando
-         if ($canDo->get('core.edit.state'))
+ 
+         /* TODo werkt nog niet $cando
+         if (!$this->isEmptyState && ($canDo->get('core.edit.state') || ($this->state->get('filter.published') == -2 && $canDo->get('core.delete'))))
          {
-         JToolbarHelper::publish('wsaonepage.publish', 'JTOOLBAR_PUBLISH', true);
-         JToolbarHelper::unpublish('wsaonepage.unpublish', 'JTOOLBAR_UNPUBLISH', true);
+        $dropdown = $toolbar->dropdownButton('status-group')
+        ->text('JTOOLBAR_CHANGE_STATUS')
+        ->toggleSplit(false)
+        ->icon('icon-ellipsis-h')
+        ->buttonClass('btn btn-action')
+        ->listCheck(true);
+        
+        $childBar = $dropdown->getChildToolbar();
+
+         
+			if ($canDo->get('core.edit.state'))
+			{
+				if ($this->state->get('filter.published') != 2)
+				{
+					$childBar->publish('banners.publish')->listCheck(true);
+
+					$childBar->unpublish('banners.unpublish')->listCheck(true);
+				}
+
+				if ($this->state->get('filter.published') != -1)
+				{
+					if ($this->state->get('filter.published') != 2)
+					{
+						$childBar->archive('banners.archive')->listCheck(true);
+					}
+					elseif ($this->state->get('filter.published') == 2)
+					{
+						$childBar->publish('publish')->task('banners.publish')->listCheck(true);
+					}
+				}
+
+				$childBar->checkin('banners.checkin')->listCheck(true);
+
+				if ($this->state->get('filter.published') != -2)
+				{
+					$childBar->trash('banners.trash')->listCheck(true);
+				}
+			}
          }
          */
-        JToolBarHelper::deleteList('', 'wsaonepages.delete');
-        JToolBarHelper::preferences('com_wsaonepages');
-        //	    JToolbarHelper::back( 'JTOOLBAR_CLOSE',  'javascript:history.back();');
+         //	    JToolbarHelper::back( 'JTOOLBAR_CLOSE',  'javascript:history.back();');
         
     }
     /**
@@ -111,21 +165,21 @@ class HtmlView extends BaseHtmlView {
     protected function getSortFields()
     {
         return array(
-            'a.ordering'     => JText::_('JGRID_HEADING_ORDERING'),
+            'a.ordering'     => Text::_('JGRID_HEADING_ORDERING'),
             //	        'a.state'        => JText::_('JSTATUS'),
-            'a.title'        => JText::_('JGLOBAL_TITLE'),
+            'a.title'        => Text::_('JGLOBAL_TITLE'),
             //	        'category_title' => JText::_('JCATEGORY'),
             //	        'access_level'   => JText::_('JGRID_HEADING_ACCESS'),
             //	        'a.created_by'   => JText::_('JAUTHOR'),
-            'language'       => JText::_('JGRID_HEADING_LANGUAGE'),
+            'language'       => Text::_('JGRID_HEADING_LANGUAGE'),
             //	        'a.created'      => JText::_('JDATE'),
-            'a.id'           => JText::_('JGRID_HEADING_ID'),
+            'a.id'           => Text::_('JGRID_HEADING_ID'),
             //	        'a.featured'     => JText::_('JFEATURED')
         );
     }protected function setDocument()
     {
-        $document = JFactory::getDocument();
-        $document->setTitle(JText::_('COM_WSAONEPAGE_ADMINISTRATION'));
+        $document = Factory::getDocument();
+        $document->setTitle(Text::_('COM_WSAONEPAGE_ADMINISTRATION'));
     }
     
 
