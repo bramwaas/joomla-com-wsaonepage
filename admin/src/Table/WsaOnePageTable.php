3e9afd3f9da2b2812ad6820605fc97069b1560c4
bram waasdorp <bramwaas@gmail.com>
2021-08-17 13:15:07 +0200

    0.6.1 Table

--- admin/src/Table/WsaOnePageTable.php
+++ admin/src/Table/WsaOnePageTable.php
@@ -6,14 +6,19 @@
  * @copyright   Copyright (C) 2020 - 2021 AHC Waasdorp. All rights reserved.
  * @license     GNU General Public License version 3 or later; see LICENSE.txt
  */
+
  namespace WaasdorpSoekhan\Component\WsaOnePage\Administrator\Controller;
 
 // No direct access
 \defined('_JEXEC') or die('Restricted access');
+
+use Joomla\CMS\Factory;
 use Joomla\CMS\Table\Table;
 use Joomla\Database\DatabaseDriver;
+use Joomla\Registry\Registry;
+
 /**
- * Hello Table class
+ * WsaOnepage Table class
  *
  * @since  0.0.1
  */
@@ -24,16 +29,21 @@ class WsaOnePageTable extends Table
 	 *
 	 * @param   DatabaseDriver  &$db  A database connector object
 	 */
-	function __construct(&$db)
+	function __construct(&$db) // TODO why &
 	{
 		parent::__construct('#__wsaonepage', 'id', $db);
+		
+		$this->created = Factory::getDate()->toSql();
+//		$this->setColumnAlias('published', 'state');
+		
+	
 	}
 	/**
 	 * Overloaded bind function
 	 *
 	 * @param       array           named array
 	 * @return      null|string     null is operation was satisfactory, otherwise returns an error
-	 * @see JTable:bind
+	 * @see Table:bind
 	 * @since 1.5
 	 */
 	public function bind($array, $ignore = '')
@@ -41,10 +51,10 @@ class WsaOnePageTable extends Table
 	    if (isset($array['params']) && is_array($array['params']))
 	    {
 	        // Convert the params field to a string.
-	        $parameter = new JRegistry;
-	        $parameter->loadArray($array['params']);
-	        $array['params'] = (string)$parameter;
+	        $registry = new Registry($array['params']);
+	        $array['params'] = (string) $registry;
 	    }
 	    return parent::bind($array, $ignore);
 	}
 }
+
