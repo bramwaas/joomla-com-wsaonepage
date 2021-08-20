0f3802d73c4a825eff8ad328ec7712fbf8e2b5db
bram waasdorp <bramwaas@gmail.com>
2021-08-20 16:10:47 +0200

    0.7.2 ucfirst 2b
    Replace WsaOnePage by Wsaonepage to accommodate ucfirst. Second in
    Filenames and directorynames

---
+++ site/src/View/Wsaonepage/HtmlView.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @package     Joomla.Administrator
+ * @subpackage  com_wsaonepage
+ *
+ * @copyright   Copyright (C) 2021 A.H.C. Waasdorp. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+namespace WaasdorpSoekhan\Component\Wsaonepage\Site\View\Wsaonepage;
+
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+
+/**
+ * View for the user identity validation form
+ */
+class HtmlView extends BaseHtmlView {
+    
+
+    /**
+     * Display the view
+     *
+     * @param   string  $template  The name of the layout file to parse.
+     * @return  void
+     */
+    public function display($template = null) {
+        // Call the parent display to display the layout file
+        parent::display($template);
+    }
+
+}
