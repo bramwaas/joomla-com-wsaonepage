f5aced4df28b495b3f270ac9afb8d69417e6537f
bram waasdorp <bramwaas@gmail.com>
2021-09-14 12:18:20 +0200

    0.9.2

---
+++ admin/sql/updates/mysql/0.9.2.sql
@@ -0,0 +1,6 @@
+--
+-- This file will contain Table structure for `"__wsaonepage`
+-- v 0.9.2 DROP some not used fields
+--
+ALTER TABLE `#__wsaonepage` DROP COLUMN `attribs`;
+ALTER TABLE `#__wsaonepage` DROP COLUMN `state`;
