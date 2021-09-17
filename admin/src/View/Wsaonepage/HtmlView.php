d2457acf62f871bdb52b1ed986d389f7fcf235ed
bram waasdorp <bramwaas@gmail.com>
2021-09-17 19:34:44 +0200

    0.9.3

--- admin/src/View/Wsaonepage/HtmlView.php
+++ admin/src/View/Wsaonepage/HtmlView.php
@@ -12,7 +12,6 @@ namespace WaasdorpSoekhan\Component\Wsaonepage\Administrator\View\Wsaonepage;
 
 \defined('_JEXEC') or die;
 
-//use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Form\Form;
