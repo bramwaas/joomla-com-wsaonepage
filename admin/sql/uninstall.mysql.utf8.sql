32609a30e31616186c7d1fd10d76b17adf0d9599
ahcwa <ahcwa@bram-len.fritz.box>
2022-10-08 15:36:33 +0200

    1.0.1

--- admin/sql/uninstall.mysql.utf8.sql
+++
@@ -1,8 +0,0 @@
---
--- This file will remove Table structure for `#__wsaonepage`
---
-DROP TABLE IF EXISTS `#__wsaonepage`;
---
---  0.9.3 delete content-history 
---
-DELETE FROM `#__content_types` WHERE `type_alias` = 'com_wsaonepage.wsaonepage';
