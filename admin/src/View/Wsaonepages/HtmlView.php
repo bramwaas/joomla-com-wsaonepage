405d90c6b2a3a81938547facbaf0e47976de1bd1
ahcwa <ahcwa@bram-len.fritz.box>
2023-12-10 18:19:05 +0100

    v1.1.2 start adaptations to J4.4. and J5

--- admin/src/View/Wsaonepages/HtmlView.php
+++ admin/src/View/Wsaonepages/HtmlView.php
@@ -5,11 +5,13 @@
  *
  * @copyright   Copyright (C) 2021 A.H.C. Waasdorp. All rights reserved.
  * @license     GNU General Public License version 3; see LICENSE
+ * 20231210 since   J 4.4.0 setDocument is part of AbstractView and public. 
  */
 namespace WaasdorpSoekhan\Component\Wsaonepage\Administrator\View\Wsaonepages;
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Document\Document;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Helper\ContentHelper;
 use Joomla\CMS\Language\Multilanguage;
@@ -179,9 +181,20 @@ class HtmlView extends BaseHtmlView {
             'a.id'           => Text::_('JGRID_HEADING_ID'),
             //	        'a.featured'     => JText::_('JFEATURED')
         );
-    }protected function setDocument()
+    }
+    /**
+     * Set the document to use.
+     *
+     * @param   Document  $document  The document to use
+     *
+     * @return  void
+     *
+     * @since   J 4.4.0 public part of AbstractView and param $document 
+     */
+    public function setDocument(Document $document = NULL)
     {
-        $document = Factory::getDocument();
+        if (is_null($document))  $document = Factory::getApplication()->getDocument();
+        $this->document = $document;
         $document->setTitle(Text::_('COM_WSAONEPAGE_ADMINISTRATION'));
     }
     
